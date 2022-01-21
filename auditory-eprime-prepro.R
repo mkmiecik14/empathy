8b33afa460ddc09471eacde9c500cdc30df6db93
Matt Kmiecik <mkmiecik14@gmail.com>
2022-01-21 17:15:33 -0600

    updated the auditory prepo script to include mean and slope output

--- auditory-eprime-prepro.R
+++ auditory-eprime-prepro.R
@@ -13,7 +13,7 @@ db_conv <- tibble(stim = 1:5, db = c(30, 45, 60, 75, 90))
 # Loads data ----
 auditory_data <- 
   read_delim(
-    file = "../data/eprime/auditory/empathy-auditory-merge-6-may-2021.txt", 
+    file = "../data/eprime/auditory/empathy-auditory-merge-21-jan-2022.txt", 
     delim = "\t", # tab delimited
     guess_max = 2000 # this is necessary due to parsing failures
   )
@@ -67,19 +67,80 @@ auditory_data_long <-
 # Test done here to prove that ratings > 20 are fixed
 # auditory_data_long %>% count(rating) %>% View()
 
+# Visualization for data quality checks ----
+# UNCOMMENT TO SEE
+# Baselines
+# this_data <- auditory_data_long %>% filter(session == 10) # change to 11 or 12
+# 
+# # Violin plot
+# pj <- position_jitter(width = .15, height = .1)
+# ggplot(this_data, aes(factor(stim), rating)) +
+#   geom_point(position = pj, alpha = 1/2) +
+#   geom_flat_violin(position = position_nudge(x = .2, y = 0), alpha = .8) +
+#   theme_minimal()
+# 
+# # linear
+# pj <- position_jitter(width = .15, height = .1)
+# ggplot(this_data, aes(stim, rating)) +
+#   geom_point(position = pj, alpha = 1/2) +
+#   geom_smooth(method = "lm", se = TRUE, color = "red") +
+#   theme_minimal()
+
+# Summary stats
+auditory_sum <- 
+  auditory_data_long %>% 
+  filter(complete.cases(rating)) %>% # removes missing data points
+  group_by(ss, session) %>%
+  summarise(m = mean(rating), n = n()) %>%
+  ungroup()
+
+# auditory_sum %>% filter(session<10) # checker
+
+# modeling slopes
+auditory_mods <-
+  auditory_data_long %>%
+  nest_by(ss, session) %>%
+  mutate(mod = list(lm(rating ~ 1 + scale(stim, scale = FALSE), data = data)))
+
+auditory_ests <-
+  auditory_mods %>%
+  summarise(broom::tidy(mod)) %>%
+  ungroup() %>%
+  mutate(
+    term = gsub("\\(Intercept\\)", "mean", term),
+    term = gsub("scale\\(stim, scale = FALSE\\)", "slope", term)
+  )
+
+# Converting this to wide for excel users
+auditory_ests_wide <-
+  auditory_ests %>%
+  pivot_wider(id_cols = c(ss, session), names_from = term, values_from = estimate)
+
 # Wide format for Excel users ----
 auditory_data_wide <- 
   auditory_data_long %>% 
   select(ss, session, stim, rating) %>%
-  pivot_wider(id_cols = c(ss, session), names_from = stim, values_from = rating)
+  pivot_wider(id_cols = c(ss, session), names_from = stim, values_from = rating) %>%
+  left_join(., auditory_ests_wide, by = c("ss", "session"))
 
 # Saves out data ----
 save(auditory_data_long, file = "../output/auditory-data-long.RData") # Rdata long
 write_csv(auditory_data_long, file = "../output/auditory-data-long.csv") # csv long
+save(auditory_data_wide, file = "../output/auditory-data-wide.RData") # RData wide
 write_csv(auditory_data_wide, file = "../output/auditory-data-wide.csv") # csv wide
 
 # Cleans up script objects ----
-rm(db_conv, auditory_data, auditory_data_long, auditory_data_wide, doubles)  
+rm(
+  db_conv, 
+  auditory_data, 
+  auditory_data_long, 
+  auditory_data_wide, 
+  doubles,
+  auditory_ests,
+  auditory_ests_wide,
+  auditory_mods,
+  auditory_sum,
+  )  
   
   
   
