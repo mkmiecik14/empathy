d39392843513ab1a0c175d44ae7a9b682d8819ac
Matt Kmiecik <mkmiecik14@gmail.com>
2022-06-07 16:26:57 -0500

    lmm analysis complete

--- analysis-2.R
+++ analysis-2.R
@@ -51,13 +51,113 @@ vis_mods <-
     min_mod = list(
       lmer(rating ~ 1 + stim_mc + psd_mc + (1 | ss), data = data, REML = TRUE)
     ),
+    psd_mod = list(
+      lmer(rating ~ 1 + stim_mc + psd_mc + (1 + psd_mc | ss), data = data, REML = TRUE)
+    ),
     max_mod = list(
       lmer(rating ~ 1 + stim_mc + psd_mc + (1 + stim_mc + psd_mc | ss), data = data, REML = TRUE)
     )
   ) %>%
   mutate(anova_mod = list(anova(min_mod, max_mod))) # compares mods
 
-# Extracting estimates
+#################
+#               #
+# Minimum Model #
+#               #
+#################
+
+min_mod_ests <- vis_mods %>% summarise(broomExtra::tidy(min_mod)) %>% ungroup()
+
+# cleans them up and inserts electrode locations
+min_mod_ests_clean <-
+  min_mod_ests %>% 
+  filter(term %in% c("(Intercept)", "stim_mc", "psd_mc")) %>%
+  mutate(
+    term = case_when(
+      term == "(Intercept)" ~ "Intercept",
+      term == "stim_mc" ~ "brightness",
+      term == "psd_mc" ~ "psd"
+    ),
+    sig = p.value < .05
+  ) %>%
+  group_by(term) %>%
+  mutate(
+    p.fdr = p.adjust(p.value, method = "fdr"),
+    sig.fdr = p.fdr<.05
+  ) %>%
+  ungroup() %>%
+  left_join(., elec_locs, by = c("elec" = "labels"))
+
+# interpolates data for topo
+min_mod_ests_clean_interp <- 
+  min_mod_ests_clean %>%
+  split(.$term) %>%
+  map_dfr(~topo_interp(data = .x, dv = "estimate", gridRes = 67), .id = "term") %>%
+  as_tibble(.)
+
+# plots psd slopes
+test <- min_mod_ests_clean %>% filter(term == "psd")
+min(test$estimate); max(test$estimate)
+topo_plot(
+  orig_data = min_mod_ests_clean %>% filter(term == "psd"), 
+  interp_data = min_mod_ests_clean_interp %>% filter(term == "psd"), 
+  dv = estimate,
+  legend_name = "PSD Slopes",
+  electrode_size = 1.5,
+  color_pal_limits = c(-.2, .2),
+  color_pal_breaks = seq(-.2, .2, .1),
+  bwidth = 2,
+  color_pal = rev(brewer.pal(n = 9, "RdGy")),
+  elec_shape_col = sig.fdr,
+  elec_shapes = c(1, 16)
+)
+
+# plots stim slopes
+test <- min_mod_ests_clean %>% filter(term == "brightness")
+min(test$estimate); max(test$estimate)
+topo_plot(
+  orig_data = min_mod_ests_clean %>% filter(term == "brightness"), 
+  interp_data = min_mod_ests_clean_interp %>% filter(term == "brightness"), 
+  dv = estimate,
+  legend_name = "Brightness Slopes",
+  electrode_size = 1.5,
+  color_pal_limits = c(-.8, .8),
+  color_pal_breaks = seq(-.8, .8, .4),
+  bwidth = 2,
+  color_pal = rev(brewer.pal(n = 9, "RdGy")),
+  elec_shape_col = sig.fdr,
+  elec_shapes = c(16)
+)
+
+# looking at psd slopes - line graphs
+min_mod_aug <- 
+  vis_mods %>% 
+  summarise(broom::augment(min_mod)) %>%
+  ungroup()
+
+this_data <- min_mod_aug %>% filter(elec==28, ss %in% c(1:75))
+ggplot(this_data, aes(psd_mc, rating, group = ss)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "black", size = .5) +
+  geom_smooth(
+    aes(psd_mc, .fitted), 
+    method = "lm", 
+    se = FALSE, 
+    linetype = 2, 
+    color = "red",
+    size = .5
+    ) +
+  labs(x = "PSD", y = "Unpleasantness", title = "Minimum Model") +
+  facet_wrap(~ss) +
+  theme_bw()
+  
+
+#################
+#               #
+# Maximum Model #
+#               #
+#################
+
 max_mod_ests <- vis_mods %>% summarise(broomExtra::tidy(max_mod)) %>% ungroup()
 
 # cleans them up and inserts electrode locations
@@ -138,7 +238,70 @@ topo_plot(
   elec_shapes = c(1, 16)
 )
 
+# looking at psd slopes - line graphs
+max_mod_aug <- 
+  vis_mods %>% 
+  summarise(broom::augment(max_mod)) %>%
+  ungroup()
+
+this_data <- max_mod_aug %>% filter(elec==28, ss %in% c(1:75))
+ggplot(this_data, aes(psd_mc, rating, group = ss)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE, color = "black", size = .5) +
+  geom_smooth(
+    aes(psd_mc, .fitted), 
+    method = "lm", 
+    se = FALSE, 
+    linetype = 2, 
+    color = "red",
+    size = .5
+  ) +
+  labs(x = "PSD", y = "Unpleasantness", title = "Maximum Model") +
+  facet_wrap(~ss) +
+  theme_bw()
+
 # Several of the models did not converge; I would make sure that including the 
 # random effects improves model fit
+summary(vis_mods$max_mod[[7]])$optinfo$conv$lme4$messages
 
+# Max models
+1:62 %>% map(~summary(vis_mods$max_mod[[.x]])$optinfo$conv$lme4$messages)
+# PSD models
+1:62 %>% map(~summary(vis_mods$psd_mod[[.x]])$optinfo$conv$lme4$messages)
+# Minimum models
+1:62 %>% map(~summary(vis_mods$min_mod[[.x]])$optinfo$conv$lme4$messages)
 
+####################
+#                  #
+# Model comparison #
+#                  #
+####################
+
+# Max vs. Min models
+mod_compare_ests <- 
+  vis_mods %>% 
+  summarise(broom::tidy(anova_mod)) %>%
+  ungroup() %>%
+  filter(term == "max_mod") %>%
+  mutate(sig = p.value<.05) %>%
+  left_join(., elec_locs, by = c("elec" = "labels"))
+
+# interpolates data for topo
+mod_compare_ests_interp <- 
+  topo_interp(data = mod_compare_ests, dv = "statistic", gridRes = 67)
+
+# plots stats
+min(mod_compare_ests$statistic); max(mod_compare_ests$statistic)
+topo_plot(
+  orig_data = mod_compare_ests, 
+  interp_data = mod_compare_ests_interp, 
+  dv = statistic,
+  legend_name = "Model Comparison \n (Chi-Square)",
+  electrode_size = 1.5,
+  color_pal_limits = c(50, 80),
+  color_pal_breaks = seq(50, 80, 5),
+  bwidth = 2,
+  color_pal = brewer.pal(n = 9, "Blues"),
+  elec_shape_col = sig,
+  elec_shapes = c(16)
+) 
