416706fa9f9763042e4779e932c5ee8fb59e07d5
Matt Kmiecik <mkmiecik14@gmail.com>
2025-04-24 22:42:38 -0700

    added PPT data to redcap join script

--- prepro/redcap_join.R
+++ prepro/redcap_join.R
@@ -8,7 +8,6 @@
 # libraries ----
 library(tidyverse)
 
-
 # data ----
 
 ## redcap data
@@ -16,7 +15,7 @@ d <- "EH17338EMPATHY_DATA_2024-11-07_1140_noPHI.csv"
 f <- file.path("data", d)
 rdcp <- read_csv(f)
 
-## task data
+## visual/ auditory task data
 f <- list.files(path = "output", pattern = "sum-data.rds")
 tmp <- vector("list", length = length(f))
 names(tmp) <- gsub("-", "_", f)               # Replace hyphens
@@ -26,14 +25,6 @@ names(tmp) <- gsub("\\.rds$", "", names(tmp)) # Remove .rds extension
 for (i in 1:length(tmp)) {
   tmp[[i]] <- readRDS(file = file.path("output", f[i]))
 }
-
-rdcp %>% filter(redcap_event_name == "annual_year_1_arm_1") %>% select(where(~ !all(is.na(.)))) %>% View()
-rdcp %>% 
-  select(record_id, subject_id, redcap_event_name, contains("date")) %>% 
-  filter(!is.na(todaysdate)) %>%
-  relocate(todaysdate, .after = redcap_event_name)
-  View()
-
 
 # combines all task data
 task_data <- 
@@ -50,10 +41,66 @@ task_data <-
     .after = date
     )
 
+## PPT data
+f <- file.path("output", "ppt-data.rds")
+ppt_data <- readRDS(file = f)
+
+# summarises PPT by subject
+ppt_ss <- 
+  ppt_data %>%
+  filter(!is.na(strain)) %>% # removes missing data
+  summarise(m = mean(strain), n = n(), .by = c(ss, session, location, task)) %>%
+  # adds redcap data to help with join
+  mutate(
+    redcap_event_name = case_when(
+      session == 0 ~ "baseline_visit_chi_arm_1",
+      session == 1 ~ "visit_1_child_arm_1",
+      session == 2 ~ "visit2_child_arm_1",
+      .default = "missing"
+    )
+  )
+
+ppt_ss_wide <- 
+  ppt_ss %>%
+  pivot_wider(
+    id_cols = c(ss, session, redcap_event_name), 
+    values_from = m, 
+    names_from = c(location, task)
+    ) %>%
+  # calculates effect of CPM
+  mutate(
+    knee_CPM_sub = knee_CPM - knee_PPT, 
+    shoulder_CPM_sub = shoulder_CPM - shoulder_PPT
+    )
+
+# examining the relationship between knee and shoulder CPM
+# uncomment to see
+# tmp <- ppt_ss_wide %>% filter(session == 0)
+# ggplot(tmp, aes(shoulder_CPM_sub, knee_CPM_sub)) + geom_point() + geom_smooth(method = "lm")
+# cor.test(tmp$knee_CPM_sub, tmp$shoulder_CPM_sub)
+#
+# I am deciding not to z-score and combine the CPM measures. This is because:
+# 1. attrition over time may not be random (dist will change)
+# 2. shoulder and knee are correlated, but modestly
+
+# prepping data for join ----
+
+## auditory / visual task data
+task_data_j <- task_data %>% select(-session, -date)
+
+## PPT / CPM data
+ppt_data_j <- 
+  ppt_ss_wide %>% 
+  select(
+    ss, redcap_event_name, knee_PPT, shoulder_PPT, 
+    knee_CPM = knee_CPM_sub, shoulder_CPM = shoulder_CPM_sub
+    )
+
 # joins with redcap
 comb <- 
   rdcp %>%
-  left_join(., task_data, by = c("subject_id" = "ss", "redcap_event_name"))
+  left_join(., task_data_j, by = c("subject_id" = "ss", "redcap_event_name")) %>%
+  left_join(., ppt_data_j, by = c("subject_id" = "ss", "redcap_event_name"))
 
 # writes out data ----
 
